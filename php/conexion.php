e3b4ef76d223363f4e8f367b81f2bbb6beee198f
Kometha <kenethcubas2002@unitec.edu>
2023-03-20 12:50:24 -0600

    Conexión.php modificada con los nuevos datos

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $server = "sql9.freesqldatabase.com";
-$username = "sql9605357";
-$password = "MEH2VXRlAn";
-$database = "sql9605357";
+$username = "sql9607296";
+$password = "fDLSZ1Vsd1";
+$database = "sql9607296";
 $port = "3306";
 
 $conexion = mysqli_connect($server, $username, $password, $database, $port);
